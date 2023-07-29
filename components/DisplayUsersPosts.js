9c0323cbcde223d1c77a453d184adab7b38a1d5a
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2023-07-29 17:30:12 -0400

    refreshing dynamic route not working

--- components/DisplayUsersPosts.js
+++ components/DisplayUsersPosts.js
@@ -1,85 +1,78 @@
 "use client"
-import React,{useState, useEffect, useContext} from 'react'
-import { Web3Context } from "../context/Web3Context"
-import PostCard from './PostCard'
-import Image from 'next/image'
+import React, { useState, useEffect, useContext } from 'react';
+import { Web3Context } from '../context/Web3Context';
+import PostCard from './PostCard';
+import Image from 'next/image';
 
-const DisplayUsersPosts = ({address}) => {
-    const [usersPosts, setUsersPosts] = useState()
-    const [isErr, setIsErr] = useState(false)
-    
 
-    const {getAllUsersPost, viewedProfile, fetchUsersProfile} = useContext(Web3Context)
+const DisplayUsersPosts = ({ address }) => {
+  const [usersPosts, setUsersPosts] = useState();
+  const [isErr, setIsErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const fetchPageData = async () =>{
-        try{
-            const data = await getAllUsersPost(address)
-            if(data == undefined) {
-                setIsErr(true)
-                return
-            }
-            setUsersPosts(data)
-            
-            setIsErr(false)
-        }catch(err){
-            console.log(err)
-            setIsErr(true)
-            
-        }
+  const { getAllUsersPost, viewedProfile, fetchUsersProfile } = useContext(Web3Context);
+
+  const fetchPageData = async () => {
+    try {
+      const data = await getAllUsersPost(address);
+      if (data == undefined) {
+        setIsErr(true);
+        setIsLoading(false);
+        return;
+      }
+      setUsersPosts(data);
+      setIsErr(false);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setIsErr(true);
+      setIsLoading(false);
     }
-    
-    useEffect(()=>{
-        fetchPageData()
-        fetchUsersProfile(address)
-        
-        
-    },[])
-    
+  };
+
+  useEffect(() => {
+    fetchPageData();
+    fetchUsersProfile(address);
+  }, []);
+
   return (
-<>
-  {!isErr ? (
-    <div className='pt-6 pl-6' >
-     
-      {viewedProfile && (
-        <div className='float-left'>
-        
-        <h2 >User: {viewedProfile.username}</h2>
-          <Image
-            src={viewedProfile.profileQRCode}
-            alt='profile qr code'
-            height={200}
-            width={200}
-          />
+    <>
+      {isLoading ? (
+        <div className='text-center'>
+          <h2 className='text-4xl'>Loading...</h2>
         </div>
-     
+      ) : !isErr ? (
+        <div className='pt-6 pl-6'>
+          {viewedProfile && (
+            <div className='float-left'>
+              <h2>User: {viewedProfile.username}</h2>
+              <Image src={viewedProfile.profileQRCode} alt='profile qr code' height={200} width={200} />
+            </div>
+          )}
+          <div className='flex flex-col items-center'>
+            <div className='flex flex-wrap justify-center space-x-2'>
+              {usersPosts.map((post, i) => {
+                return (
+                  <PostCard
+                    key={i}
+                    ipfsHash={post.ipfsHash}
+                    qrCodeSvg={post.qrCodeSvg}
+                    postNumber={post.postNumber}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className='text-center'>
+          <h2 className='text-4xl'>NO USER PROFILE FOUND</h2>
+        </div>
       )}
-    <div className='flex flex-col items-center'>
+    </>
+  );
+};
 
-      <div className='flex flex-wrap justify-center space-x-2'>
-        {usersPosts &&
-          usersPosts.map((post, i) => {
-            return (
-              <PostCard
-                key={i}
-                ipfsHash={post.ipfsHash}
-                qrCodeSvg={post.qrCodeSvg}
-                postNumber={post.postNumber}
-              />
-            );
-          })}
-      </div>
-    </div>
-    </div>
-  ) : (
-    <div className='text-center'>
-      <h2 className='text-4xl'>NO USER FOUND</h2>
-    </div>
-  )}
-</>
 
-  
 
-  )
-}
-
-export default DisplayUsersPosts
+export default DisplayUsersPosts;
--- src/app/findposts/[address]/page.js
+++ src/app/findposts/[address]/page.js
@@ -2,13 +2,17 @@ import React from 'react'
 import DisplayUsersPosts from '../../../../components/DisplayUsersPosts'
 
 
+
 const page = ({params}) => {
   const address = params.address
+  
   return (
     <div>
-      <DisplayUsersPosts address={address} />
+      <DisplayUsersPosts address={address} />   
     </div>
   )
 }
+
+  
 
 export default page
